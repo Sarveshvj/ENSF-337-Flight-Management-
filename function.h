3109b56bde7b05a1529ba98f2c8f35bffb1e8d6a
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2025-11-26 12:59:14 -0700

    added the initial classes with partial definitions

---
+++ function.h
@@ -0,0 +1,65 @@
+#include <vector>
+#include <string>
+class Passenger{
+    private:
+    string firstname;
+    string lastname;
+    string phone;
+    int id;
+    int row;
+    char seat;
+
+    public:
+    // Getters Setters Ctors
+};
+
+class Seat {
+    private:
+    int rowNumber;
+    char seatChar;
+    Passenger* occupant;
+
+    public:
+    // Getters Setters Ctors
+    bool isEmpty() const {return occupant == nullptr;}
+    void assignPassenger(Passenger* p) {occupant = p;}
+    void removePassenger(Passenger* p) {occupant = nullptr;}
+};
+
+class Route {
+    private:
+    string source;
+    string dest;
+
+    public:
+    // Getters Setters Ctors
+};
+
+class Airline {
+    private:
+    string name;
+    vector<Flight*> flights;
+
+    public:
+    // Getters Setters Ctors
+    void flightsFromfile(const string& filename);
+    void passengerFromfile(const string& filename);
+    Flight* findFlight(string& id);
+    void addFlight(Flight& flight)
+};
+
+class Flight {
+    private:
+    string flightID;
+    int numRows;
+    int seatsPerRow;
+    Route* route;
+    vector<vector<Seat*>> seats;
+    
+    public:
+    bool seatAvailable(int row, char col) const;
+    void addPassenger(Passenger& p);
+    void removePassenger(int id);
+    void displaySeatMap() const;
+    void displayPassengers() const;
+};
