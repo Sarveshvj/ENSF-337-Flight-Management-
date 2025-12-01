17fa54ac59b33d70962bff49e3b20d23e4c3a819
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2025-12-01 11:52:44 -0700

    fixed formatting for comments

--- Flight.h
+++ Flight.h
@@ -40,33 +40,32 @@ public:
 
     // Helper / other functions
 
+    void addPassengerFromFile(Passenger& p);
     //REQUIRES: A passenger object
     //PROMISES: Checks the validity of the row and seat letter for the passenger
     // If the passenger is valid the seat the passenger would like is initialized on heap
     // and the passenger is then assigned to that seat
-    void addPassengerFromFile(Passenger& p);
 
+    void addPassenger();
     // PROMISES: Prompts user for information about added passenger, along with this it will error check inputs
     // Creates a new passenger on the heap, if the desiered seat is not initialized or taken then passenger is put in that seat
     // If seat is taken then it lets user know that the seat is occupied
-    void addPassenger();
 
+    void removePassenger();
     // PROMISES: Prompts user for the id of passenger they would like to remove
     // System then scans through seats vector trying to find id
     // If the ID is found the passenger is deleted and pointer is set to nullptr
     // If passenger ID not found prompt again for a valid ID
-    void removePassenger();
 
-    //PROMISES: Displays the seat map in same template as given to us
     void displaySeatMap() const;
+    //PROMISES: Displays the seat map in same template as given to us
 
+    void displayPassengers() const;
     //PROMISES: Displays the passengers in same template as given to us
     // in order of ID of the passengers 
-    void displayPassengers() const;
-    
+
+    int seatindex(char col);
     //PROMISES: Makes sure the char is any letter A-F, otherwise return is -1
     //If char is valid will return assigned index value of char
-    int seatindex(char col);
 };
-
 #endif
